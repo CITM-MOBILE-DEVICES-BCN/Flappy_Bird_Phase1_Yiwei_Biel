9b6c9d5f2c74790ef30bcf890ccd21fb0b541d45
Yeeway69 <yeyiwei1@gmail.com>
2024-10-05 16:37:46 +0200

    BackgroundMusic Plays

---
+++ Flappy Bird Tutorial Practice/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+
+    [SerializeField] private AudioSource backgroundMusicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("---------- Audio Clips ----------")]
+    public AudioClip backgroundMusic;
+    public AudioClip point;
+    public AudioClip hit;
+    public AudioClip die;
+    public AudioClip wing;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayBackgroundMusic(backgroundMusic, transform, 0.5f);
+    }
+
+    private void PlayBackgroundMusic(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(backgroundMusicSource, spawnTransform.position, Quaternion.identity);
+
+        backgroundMusicSource.clip = backgroundMusic;
+
+        backgroundMusicSource.volume = volume;
+
+        backgroundMusicSource.Play();
+
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(sfxSource, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioClip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    
+
+
+
+}
